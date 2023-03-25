e832cb925806dc50a7fef7bec30c7573c145ad46
katakume <72819680+katakume-astro@users.noreply.github.com>
2023-03-25 16:29:51 +0100

    Update anticheat.lua

--- katakume-strefy/client/anticheat.lua
+++ katakume-strefy/client/anticheat.lua
@@ -1,31 +1,21 @@
-    local przejmowanie = false
-local time = 300
+local przejmowanie = false
 
-
-
-
-CreateThread(function()
-    
+CreateThread(function() 
     local point = lib.points.new(vec3(58.3158, 3715.3621, 39.7549), 6, {
         strefa = 'strefa',
     })
-    
+
     function point:onEnter()
-    
         lib.showTextUI('[E] - Przejmij strefa')
-    
     end
     
     function point:onExit()
-    
         exports['astro_lib']:HideText()
         przejmowanie = false
-    
     end
     
     function point:nearby()
         DrawMarker(23, self.coords.x, self.coords.y, self.coords.z-0.7, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 10.0, 10.0, 10.0, 200, 20, 20, 50, false, true, 2, nil, nil, false)
-    
         if self.currentDistance < 1 and IsControlJustReleased(0, 38) then
             przejmowanie = true
             for i = 1, 300, 1 do
