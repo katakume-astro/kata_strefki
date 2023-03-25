7c58168cc9582cb5b660cffc77467163d5630b15
katakume <72819680+katakume-astro@users.noreply.github.com>
2023-03-25 16:33:02 +0100

    Update anticheat.lua

--- katakume-strefy/server/anticheat.lua
+++ katakume-strefy/server/anticheat.lua
@@ -11,11 +11,11 @@ end
 lib.callback.register('strefy', function()
     local state = GlobalState
     local cupka = math.random(1, 3)
-	local money = math.random(1000, 5000)
+    local money = math.random(1000, 5000)
     local money2 = math.random(30, 60)
+
     if state == false then
-    -- https://docs.esx-framework.org/legacy/Client/functions/shownotification
-	exports.ox_inventory:AddItem(source, 'money', money)
+    exports.ox_inventory:AddItem(source, 'money', money)
     exports['astro_lib']:log(source, 'Gracz ojebal strefke')
     if cupka == 1 then
         exports.ox_inventory:AddItem(source, 'ammo-9', money2)
@@ -27,6 +27,6 @@ lib.callback.register('strefy', function()
         exports.ox_inventory:AddItem(source, 'ammo-50', money2)
         TimerThread()
     end
-else TriggerClientEvent('esx:showNotification',source, 'ta strefa jest juz przejeta', 'error')
+    else TriggerClientEvent('esx:showNotification',source, 'ta strefa jest juz przejeta', 'error')
     end
 end)
